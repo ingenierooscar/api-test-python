e846e3bed70539b653830bfd7b22b30552ed2363
orodriguez <orodriguez@resurety.com>
2023-04-21 17:51:03 -0500

    the last test cases

--- test_todo_api.py
+++ test_todo_api.py
@@ -1,5 +1,5 @@
 import requests
-
+import uuid
 ENDPOINT = "https://todo.pixegami.io/"
 
 """response = requests.get(ENDPOINT)
@@ -60,6 +60,35 @@ def test_can_update_task():
     assert get_task_data["is_done"] == new_payload["is_done"]
 
 
+def test_can_list_tasks():
+    # create N task
+    n = 3
+    payload = new_task_payload()
+    for _ in range(n):
+        create_task_response = create_task(payload)
+        assert create_task_response.status_code == 200
+    user_id = payload["user_id"]
+    list_tasks_response = list_task(user_id)
+    assert list_tasks_response.status_code == 200
+    data = list_tasks_response.json()
+    tasks = data["tasks"]
+    assert len(tasks) == n
+    print(data)
+
+
+def test_can_delete_task():
+    payload = new_task_payload()
+    create_task_response = create_task(payload)
+    assert create_task_response.status_code == 200
+
+    task_id = create_task_response.json()["task"]["task_id"]
+    delete_task_response = delete_task(task_id)
+    assert delete_task_response.status_code == 200
+
+    get_task_response = get_task(task_id)
+    assert get_task_response.status_code == 404
+
+
 def create_task(payload):
     return requests.put(ENDPOINT + "/create-task", json=payload)
 
@@ -72,9 +101,20 @@ def get_task(task_id):
     return requests.get(ENDPOINT + f"/get-task/{task_id}")
 
 
+def list_task(user_id):
+    return requests.get(ENDPOINT + f"/list-tasks/{user_id}")
+
+
+def delete_task(task_id):
+    return requests.delete(ENDPOINT + f"/delete-task/{task_id}")
+
+
 def new_task_payload():
+    user_id = f"test_user_{uuid.uuid4().hex}"
+    content = f"test_content_{uuid.uuid4().hex}"
+    print(f"my new user {user_id} and content {content}")
     return {
-        "content": "gonorrea",
-        "user_id": "100",
+        "content": content,
+        "user_id": user_id,
         "is_done": False,
     }
